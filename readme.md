e1cbb730543177d2ba180a9a592a84f22553113e
Razvan Nicu <cromozooom@gmail.com>
2022-12-19 11:56:48 +0000

    clean

---
+++ readme.md
@@ -0,0 +1,22 @@
+## Install
+
+1. [install](https://gohugo.io/installation/) hugo
+1. clone repo
+1. run `npm i`
+1. run `hugo server`
+
+## How to
+
+1. Edit _content_index.md_ to switch on/off **tailwind** or **bootstrap**
+
+   ```
+   tailwind: true
+   alpine: true
+   bootstrap: true
+   jquery: true
+   ```
+
+1. Add you html code in **layouts\index.html**
+1. Check the style **generated**
+
+📣 _I don't recommend to use both unless you don't have a good reason_
--- themes/simply_hugo/assets/css/postcss.config.js
+++ themes/simply_hugo/assets/css/postcss.config.js
@@ -1,13 +1,18 @@
-const themeDir = __dirname + '/../../'
+const themeDir = __dirname + '/../../';
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
-  plugins: [
-    require('postcss-import')({
-      path: [themeDir],
-    }),
-    require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
-    require('autoprefixer')({
-      path: [themeDir],
-    }),
-  ],
-}
+	plugins: [
+		require('postcss-import')({
+			path: [themeDir],
+		}),
+		require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
+		require('autoprefixer')({
+			path: [themeDir],
+		}),
+
+		purgecss({
+			content: ['./**/*.html'],
+		}),
+	],
+};
